358a551a5fbc363dc2727dacf9fdba7877d1c91e
NataliaZolotareva <NataliaZolotareva@AKUVFA0026>
2024-11-12 20:07:06 -0500

    cleanup

--- ASPNetCoreWebAPI/Controllers/WeatherForecastController.cs
+++ ASPNetCoreWebAPI/Controllers/WeatherForecastController.cs
@@ -1,10 +1,8 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web.Resource;
 
 namespace ASPNetCoreWebAPI.Controllers
 {
-    [Authorize]
     [ApiController]
     [Route("[controller]")]
     [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
